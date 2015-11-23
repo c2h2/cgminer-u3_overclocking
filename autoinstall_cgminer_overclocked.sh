6300d182314854f906cbceb39ac045225c6b2ff0
Yiling Cao <yiling.cao@gmail.com>
2015-11-23 11:30:56 +0800

    Update autoinstall_cgminer_overclocked.sh

--- autoinstall_cgminer_overclocked.sh
+++ autoinstall_cgminer_overclocked.sh
@@ -1,4 +1,5 @@
-apt-get install -y libusb-1.0-0-dev libusb-1.0-0 libcurl4-openssl-dev libncurses5-dev libudev-dev screen libtool automake pkg-config libjansson-dev screen git autoconf libtool libcurl4-openssl-dev libncurses5-dev pkg-config yasm make
+sudo apt-get update
+sudo apt-get install -y libusb-1.0-0-dev libusb-1.0-0 libcurl4-openssl-dev libncurses5-dev libudev-dev screen libtool automake pkg-config libjansson-dev screen git autoconf libtool libcurl4-openssl-dev libncurses5-dev pkg-config yasm make
 
 cd /opt/
 # git clone git://github.com/ckolivas/cgminer.git
